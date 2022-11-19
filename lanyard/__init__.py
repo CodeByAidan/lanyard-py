923a9626d968699badf61f2c77f75a9ee991d47c
SawshaDev <contactcallimarie@gmail.com>
2022-11-19 12:27:35 -0800

    Fuck forgot something?

--- lanyard/__init__.py
+++ lanyard/__init__.py
@@ -1,2 +1,8 @@
+__title__ = 'lanyard'
+__authors__ = 'SawshaDev'
+__version__ = '1.0.0'
+__license__ = 'MIT'
+__copyright__ = 'Copyright (c) 2022 SawshaDev'
+
 from .gateway import *
 from .http import *
